11d741119274d30314afeec369315587ac1933ba
Surabhi Khachar <surabhikhachar@ku.edu>
2018-10-23 20:38:52 -0500

    Pushing from local

---
+++ EECS_731_Project_4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is  using regression models to determine scores for teams on an NBA data set taken from FiviveThirtyEight. ',
+    author='Surabhi Khachar',
+    license='',
+)
